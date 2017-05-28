b45d90245400810d70a0198b47b6a24e69fa676f
thinkholic <mailtokasun@gmail.com>
2017-05-28 19:50:47 +0530

    Add entry controller

---
+++ app/Http/Controllers/EntryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EntryController extends Controller
+{
+    /*
+     * EntryController constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * List all donatiaons
+     *
+     * @return $this
+     */
+    public function view()
+    {
+        return view('/frontend/entry/view');
+    }
+}
