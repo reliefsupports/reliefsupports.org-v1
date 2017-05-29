263780c8095a52e146d6e9a990bebed04d12cdf0
Shashika Muramudalige <shashikamuramudalige@gmail.com>
2017-05-29 12:15:45 +0530

    Added captcha to forms

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -52,13 +52,23 @@ class DonationController extends Controller
      */
     public function save(Request $request)
     {
+        $messages = [
+            'name.required' => 'නම ඇතුලත් කිරීම අනිවාර්යයි',
+            'telephone.required' => 'දුරකථන අංක ඇතුලත් කිරීම අනිවාර්යයි',
+            'address.required' => 'ලිපිනය ඇතුලත් කිරීම අනිවාර්යයි',
+            'city.required' => 'නගරය/ප්‍රා.ලේ කොට්ටාසය ඇතුලත් කිරීම අනිවාර්යයි',
+            'needs.required' => 'අවශ්‍යතා ඇතුලත් කිරීම අනිවාර්යයි',
+            'g-recaptcha-response.required' => 'ඔබව තහවුරු කිරීම අනිවාර්යයි'
+        ];
+
         $validator = Validator::make($request->all(), [
             'name' => 'required|max:100',
             'telephone' => 'required|max:100',
             'address' => 'required|max:100',
             'city' => 'required|max:50',
             'donation' => 'required',
-        ]);
+            'g-recaptcha-response' => 'required|captcha'
+        ], $messages);
         
         if ($validator->fails()) {
             return redirect('/donations/add')
