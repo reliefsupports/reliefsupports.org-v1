259fa9b1a43ab726de55a638ce0ea237db8342d3
Janith Leanage <janithleanage@gmail.com>
2017-05-29 20:52:55 +0530

    added interface and information strings in sinhala, english and tamil

---
+++ resources/lang/ta/interface.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Main Web Interface Strings
+    |--------------------------------------------------------------------------
+    |
+    */
+    
+    'main_menu' => [
+		'front' => 'Front Page',
+		'aid'	=> 'Aid',
+		'needs' => 'Needs',
+		'ess_phone_nos' => 'Essential Phone Numbers',
+		'twitter_feed' 	=> '#FloodSL Twitter Feed'
+    ],
+    
+    'details' => [
+		'name' 		=> 'Name',
+		'address'	=> 'Address',
+		'city'		=> 'City',
+		'phone'		=> 'Telephone',
+		'contactable_phone' => 'Contactable Phone Number',
+		'town_dsd'	=> 'Nearest Town/DS Division',
+		'additional'=> 'Additional Info',
+		'aid'		=> 'A Description of the Aid You Can Offer',
+		'extra'		=> 'Extra details (If Any)',
+		'group'		=> 'Group Size',
+		'group_size'=> 'No. of People With You',
+		'entered_by'=> 'Entered By'
+	],
+	
+	'general' => [
+		'latest'	=> 'Latest',
+		'see_all_needs'	=> 'See All Needs',
+		'see_all_aid'	=> 'See All Aid',
+		'add_needs'		=> 'Add Needs',
+		'add_aid'		=> 'Add Aid',
+		'your' 		=> 'Your',
+		'submit'	=> 'Submit',
+		'tweets' 	=> 'Tweets about #FloodReliefLKA, #FloodSL'
+	]
+];
