f09b729da30bfb947e4afa062736b0425a3e148e
Sanath Samarasinghe <sanath.samarasinghe@outlook.com>
2017-05-30 00:02:21 +0530

    remove unwanted modal

--- resources/views/frontend/suppliers/index.blade.php
+++ resources/views/frontend/suppliers/index.blade.php
@@ -49,35 +49,4 @@
 
     </div><!-- /.container -->
 
-    <div class="modal fade"  id="donationModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="title"></h4>
-                </div>
-                <div class="modal-body">
-                    <dl class="dl-horizontal">
-                        <dt>නම</dt>
-                        <dd id="name"></dd>
-                        <dt>ආධාරය</dt>
-                        <dd id="donation"></dd>
-                        <dt>ලිපිනය</dt>
-                        <dd id="address"></dd>
-                        <dt>නගරය</dt>
-                        <dd id="city"></dd>
-                        <dt>දුරකථන අංක</dt>
-                        <dd id="telephone"></dd>
-                        <dt>අමතර විස්තර </dt>
-                        <dd id="information"></dd>
-                        <dt>ඇතුල්කලේ</dt>
-                        <dd id="added"></dd>
-                    </dl>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
 @endsection
