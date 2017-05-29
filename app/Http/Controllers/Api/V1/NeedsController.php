c6f1d9b1eda340cf383fcd0daee6d2c3615ccad3
Yoosuf <mayoosuf@gmail.com>
2017-05-29 12:01:56 +0530

    API initiative

---
+++ app/Http/Controllers/Api/V1/NeedsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Repositories\NeedsRepository;
+use Illuminate\Http\Request;
+
+class  NeedsController extends ApiController {
+
+
+    private $needs;
+
+    public function __construct(NeedsRepository $needs)
+    {
+        $this->needs = $needs;
+    }
+
+
+    /**
+     * Get All request with a default limit, or send additional requests limit and query as q
+     *
+     * @param Request $request
+     * @param null $limit
+     * @return mixed
+     */
+    public function index(Request $request, $limit = null)
+    {
+        if ($request->has('limit')) {
+            $limit = $request->get('limit');
+        }
+
+        $data = $this->needs;
+
+        $data = $data->getNeeds($limit);
+
+        return $this->respond($data);
+    }
+
+
+    /**
+     * Get Need by ID
+     *
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
+    public function show(Request $request, $id)
+    {
+        $data = $this->needs->findNeed($id);
+
+        return $this->respond($data);
+    }
+
+
+    /**
+     * Create via Needs via API
+     *
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+
+        
+
+    }
+
+}
