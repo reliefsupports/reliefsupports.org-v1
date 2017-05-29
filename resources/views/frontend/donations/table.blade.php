013448751051d852f7f9b2682ef455bd7db2ea94
Janith Leanage <janithleanage@gmail.com>
2017-05-29 20:53:52 +0530

    modified blade files to use language strings, moved donation and needs tables to seperate blade file for reuse

---
+++ resources/views/frontend/donations/table.blade.php
@@ -0,0 +1,27 @@
+<table class="table table-responsive" id="donations-table">
+    <thead>
+    <tr>
+        <th>{{ __('interface.details.name') }}</th>
+        <th>{{ __('interface.main_menu.aid') }}</th>
+        <th>{{ __('interface.details.address') }}</th>
+        <th>{{ __('interface.details.city') }}</th>
+        <th>{{ __('interface.details.phone') }}</th>
+        <th>{{ __('interface.details.entered_by') }}</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($donations as $donation)
+        <tr>
+            <th scope="row">{{ $donation->id }}</th>
+            <td>{{ $donation->name }}</td>
+            <td>{{ str_limit($donation->donation, 150) }}</td>
+            <td>{{ str_limit($donation->address, 150) }}</td>
+            <td>{{ $donation->city }}</td>
+            <td>{{ $donation->telephone }}</td>
+            <td>{{ $donation->created_at }}</td>
+            <td><button type="button" class="btn btn-primary read-donation" data-id="{{ $donation->id }}">Read full</button></td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
