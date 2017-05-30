d136f8e487dde68ef356cc4a4a30f250be79d096
Ind <thinkholic@users.noreply.github.com>
2017-05-30 23:46:47 +0530

    Revert "Adds feature related to issue #3"

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -2,22 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\DonationRepository;
-use App\Repositories\NeedsRepository;
 use Illuminate\Http\Request;
 
 class EntryController extends Controller
 {
-    private $donation;
-    private $need;
+    // private $need;
 
     /*
      * EntryController constructor.
      */
-    public function __construct(DonationRepository $donationRepository, NeedsRepository $needsRepository)
+    public function __construct()
     {
-         $this->donation = $donationRepository;
-         $this->need = $needsRepository;
     }
 
     /**
@@ -25,15 +20,8 @@ class EntryController extends Controller
      *
      * @return $this
      */
-    public function view($type, $id)
+    public function view()
     {
-        if (strcmp($type, "donation")==0){
-            $entry =  $this->donation->getDonations();
-            return view('/frontend/entry/donation') -> with ('entry', $entry);
-        } elseif(strcmp($type, "need")==0){
-            $entry =  $this->need->getNeeds();
-            return view('/frontend/entry/need') -> with ('entry', $entry);
-        }
-        return $this->donation->getDonations();
+        return view('/frontend/entry/view');
     }
 }
