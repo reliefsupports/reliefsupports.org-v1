c5115b7d17cac09f9992a70d3fc943a0ad328c00
Gayan <gayan.hewa@bigcommerce.com>
2017-05-30 15:14:33 +1000

    Add the tamil content for the online donations

--- resources/views/frontend/donations/onlineDonation.blade.php
+++ resources/views/frontend/donations/onlineDonation.blade.php
@@ -23,6 +23,12 @@
 							මෙතෙක් සිදු කළ දායකත්වයන් <a href="http://pat.as/reliefdata">මෙම සබැදිය</a> භාවිතයෙන් බලා ගත හැක.
 							</p>
 						</li>
+						<li class="list-group-item" style="padding: 2%; font-size: 40px">
+							<p>
+							வெளிநாடுகளில் வசிக்கும் நீங்களும், உங்கள் நன்கொடைகளை IMCD மூலமாக takas.lk உடன் அனுப்பி வைக்க முடியும். <a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">அனுப்பி வைக்க</a>
+							</p>
+							<p><a href="http://imcds.org/relief/" target="_blank">IMCD</a> இட்கு இதுவரை அண்ணளவாக Rs 600,000 ஒன்லைன் ஊடக நன்கொடை செய்யப்பட்டுள்ளது. #FloodReliefLKA ஊடக மேலதிக முயற்ச்சிகளும் மேற்கொள்ளப்பட்டுள்ளன. இதுவைர அளிக்கப்பட நன்கொடை சம்பந்தமான மேலதிக விபரங்களுக்கு இங்கே <a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">click</a> செய்யவும்.
+						</li>
 					</ul>
 			</div>
 		</div>
