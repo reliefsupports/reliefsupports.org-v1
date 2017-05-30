99e9cbfc9dacc1dddb6d61cf1c8943dfa5443454
Naveen Ranaweera <naveenmadhawa@gmail.com>
2017-05-29 21:15:54 -0500

    Remove kapruka link from online donation list

--- resources/views/frontend/donations/onlineDonation.blade.php
+++ resources/views/frontend/donations/onlineDonation.blade.php
@@ -8,7 +8,6 @@
                 <ul class="list-group">
                     <li class="list-group-item" style="padding: 2%; font-size: 40px"><a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">takas.lk</a></li>
                     <li class="list-group-item" style="padding: 2%; font-size: 40px"><a href="http://imcds.org/about-us" target="_blank">ICMD</a></li>
-                    <li class="list-group-item" style="padding: 2%;font-size: 40px"><a href="http://www.kapruka.com/contactUs/donate.jsp" target="_blank">kapruka.com</a></li>
                 </ul>
               <div>
         <div>
