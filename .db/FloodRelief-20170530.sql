bcdb4e25eae28c18935be4163010c070157cd67c
thinkholic <mailtokasun@gmail.com>
2017-05-31 20:46:54 +0530

    Update database tables for android

--- .db/FloodRelief-20170530.sql
+++ .db/FloodRelief-20170530.sql
@@ -106,6 +106,82 @@ INSERT INTO `needs` VALUES (1,'Janaka','0772985381','කැලණිය','කැ
 UNLOCK TABLES;
 
 --
+-- Table structure for table `device_token_details`
+--
+
+CREATE TABLE `device_token_details` (
+  `device_token_id` int(11) NOT NULL,
+  `device_token_text` longtext,
+  `device_token_user_id` int(11) DEFAULT NULL,
+  `device_token_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = active, 0 = inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `disaster_area_details`
+--
+
+CREATE TABLE `disaster_area_details` (
+  `disaster_area_id` int(11) NOT NULL,
+  `disaster_area_name` varchar(255) DEFAULT NULL,
+  `disaster_area_lat` varchar(255) NOT NULL,
+  `disaster_area_lng` varchar(255) NOT NULL,
+  `disaster_area_reason` longtext,
+  `disaster_area_special_note` longtext,
+  `disaster_area_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = active, 0 = inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `notification_details`
+--
+
+CREATE TABLE `notification_details` (
+  `notification_id` int(11) NOT NULL,
+  `notification_main_value` varchar(255) DEFAULT NULL,
+  `notification_sub_value_1` varchar(255) DEFAULT NULL,
+  `notification_sub_value_2` varchar(255) DEFAULT NULL,
+  `notification_sub_value_3` varchar(255) DEFAULT NULL,
+  `notification_sub_value_4` varchar(255) DEFAULT NULL,
+  `notification_date_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `notification_active` int(1) NOT NULL DEFAULT '1' COMMENT '1 = active,  0 = inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `device_token_details`
+--
+ALTER TABLE `device_token_details`
+  ADD PRIMARY KEY (`device_token_id`);
+
+--
+-- Indexes for table `disaster_area_details`
+--
+ALTER TABLE `disaster_area_details`
+  ADD PRIMARY KEY (`disaster_area_id`);
+
+--
+-- Indexes for table `notification_details`
+--
+ALTER TABLE `notification_details`
+  ADD PRIMARY KEY (`notification_id`);
+
+--
+-- AUTO_INCREMENT for table `device_token_details`
+--
+ALTER TABLE `device_token_details`
+  MODIFY `device_token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT for table `disaster_area_details`
+--
+ALTER TABLE `disaster_area_details`
+  MODIFY `disaster_area_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notification_details`
+--
+ALTER TABLE `notification_details`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- Table structure for table `users`
 --
 
