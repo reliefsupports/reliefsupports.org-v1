260b028a21b3f060a5c461eb26481d98dcfc854d
Chathula <schathula@gmail.com>
2017-05-31 18:21:33 +0530

    cleaned master layout, added css/js to mix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,55 +4,23 @@
     <meta charset="utf-8">
     <title>Relief Supports Sri Lanka</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Facebook Open Graph Meta Tags -->
     <meta property="og:title" content="Relief Supports Sri Lanka"/>
     <meta property="og:image" content=""/>
     <meta property="og:site_name" content="Relief Supports Sri Lanka"/>
     <meta property="og:description" content=" අයහපත් කාලගුණ තත්වය හේතුවෙන් ආපදාවට පත්වූ ඔබේ අවශ්‍යතා සහ මේ මොහොතේ ඔවුන්ට උපකාර කිරීමට සූදානම් ඔබත් මුනගැස්සවන වෙබ් සේවාවක්. ඔබේ අවශ්‍යතා සහ ආධාර දැන්ම එක් කරන්න. Post your relief support activities and connect with people who need it the most. This is a community driven Open Source project to support relief support activities conducted by volunteers. This project was originally created to support flood relief activities in Sri Lanka in May 2017."/>
-    
-    <!-- Loading Bootstrap -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,700" rel="stylesheet">
- 
-    <!-- Loading Flat UI -->
-    <link href="/public/css/flat-ui.min.css" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
     <!--[if lt IE 9]>
-    <script src="/public/js/vendor/html5shiv.js"></script>
-    <script src="/public/js/vendor/respond.min.js"></script>
+    <script src="{{ asset('js/vendor/html5shiv.js') }}"></script>
+    <script src="{{ asset('js/vendor/respond.min.js') }}"></script>
     <![endif]-->
 </head>
 <body>
-
-<style>
-    * {
-        font-family: 'Abhaya Libre', serif;
-    }
-
-    body {
-        padding-top: 95px;
-    }
-
-    .main-container {
-        min-height: 600px;
-    }
-
-    footer {
-        margin-top: 80px;
-    }
-
-    .share-buttons img {
-        width: 35px;
-        padding: 5px;
-        border: 0;
-        box-shadow: 0;
-        display: inline;
-    }
-</style>
-
 <!-- Static navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top navbar-lg" role="navigation">
     <div class="container">
@@ -91,7 +59,7 @@
                 <div class="footer-banner">
                     <h3 class="footer-title">Disclaimer</h3>
                     <p>මෙහි ඇති එක් එක් තොරතුරු ඒ ඒ පුද්ගලයන් විසින් වෙබ් අඩවියට ඇතුලත් කර ඇති අතර ඒවයේ නිරවද්‍යතාවය සම්බන්ධයෙන් සොයාබලා කටයුතු කරන ලෙස කාරුණිකව ඉල්ලා සිටිමු.</p>
-                    <p>මෙම වෙබ් සේවාව ස්වේච්ඡා සේවාවක් සහ ව්‍යපෘතියකි. තවත් තොරතුරු සහ වැඩි දියුණු කිරීම සඳහා උදවු කිරීමට 
+                    <p>මෙම වෙබ් සේවාව ස්වේච්ඡා සේවාවක් සහ ව්‍යපෘතියකි. තවත් තොරතුරු සහ වැඩි දියුණු කිරීම සඳහා උදවු කිරීමට
 <a href="https://github.com/reliefsupports/reliefsupports.org">Github පිටුවට</a></p>
                 </div>
             </div>
@@ -99,14 +67,11 @@
     </div>
 </footer>
 
-<!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
-
-<script src="/public/js/vendor/jquery.min.js"></script>
-
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="/public/js/vendor/video.js"></script>
-<script src="/public/js/flat-ui.min.js"></script>
-<script src="/public/js/donations.js"></script>
-
+<script>
+window.Laravel = {!! json_encode([
+    'csrfToken' => csrf_token(),
+]) !!};
+</script>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
