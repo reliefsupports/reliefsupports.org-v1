7b68d914b1a8725d878ecb123feb5ebc63e42a41
Dansihka Navin <danishka@gmail.com>
2017-06-06 02:24:41 +0800

    Fixed missing variable

--- resources/views/frontend/donations/add.blade.php
+++ resources/views/frontend/donations/add.blade.php
@@ -59,7 +59,7 @@
                     </div>
                     <div class="form-group">
                         <div class="col-lg-offset-2 col-lg-10">
-                            <button type="submit" class="btn btn-primary">{{ __('layout.search-placeholder.save_form') }} </button>
+                            <button type="submit" class="btn btn-primary">{{ __('home.save_form')}} </button>
                         </div>
                     </div>
                 </form>
