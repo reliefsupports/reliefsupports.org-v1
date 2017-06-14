bda57e80d4bbcac8552b28a30656c6edda2687d5
Naveen Ranaweera <naveenmadhawa@gmail.com>
2017-06-14 12:18:39 -0500

    Update content on online donation page #200

--- resources/views/frontend/donations/onlineDonation.blade.php
+++ resources/views/frontend/donations/onlineDonation.blade.php
@@ -8,7 +8,7 @@
 					<ul class="list-group">
 						<li class="list-group-item" style="padding: 2%; font-size: 40px">
 							<p>
-								<a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">Click here</a> to direct your donations via <a href="http://imcds.org/relief/" target="_blank">IMCDS</a> with the support of <a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">takas.lk</a>
+								<a href="http://takas.lk/tag/product/list/tagId/146" target="_blank">Click here</a> to direct your donations via <a href="http://imcds.org/relief/" target="_blank">IMCDS</a> with the support of <a href="http://takas.lk/tag/product/list/tagId/146" target="_blank">takas.lk</a>
 							</p>
 							<p>
 							Almost Rs. 600,000 donated online to aid IMCD's #FloodReliefLKA efforts and more coming in!
@@ -17,17 +17,17 @@
 						</li>
 						<li class="list-group-item" style="padding: 2%; font-size: 40px">
 							<p>
-								පිටරට සිටින ඔබටත් පහසුවෙන්ම දායක විය හැකි ක්‍රමයක්, <a href="http://takas.lk/tag/product/list/tagId/196" target="_blank"> දායකත්වය දැක්විමට මෙම සබැදිය භාවිත කරන්න.</a>
+								පිටරට සිටින ඔබටත් පහසුවෙන්ම දායක විය හැකි ක්‍රමයක්, <a href="http://takas.lk/tag/product/list/tagId/146" target="_blank"> දායකත්වය දැක්විමට මෙම සබැදිය භාවිත කරන්න.</a>
 							</p>
-							<p>ඔබේ දායකත්වය <a href="http://imcds.org/relief/" target="_blank">IMCDS</a> ආයතනය හරහා <a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">takas.lk</a> තාක්ෂණික සහයොගිත්වයෙන් විපතටපත් ජනතාවට ලබා දෙනු ඇත.
+							<p>ඔබේ දායකත්වය <a href="http://imcds.org/relief/" target="_blank">IMCDS</a> ආයතනය හරහා <a href="http://takas.lk/tag/product/list/tagId/146" target="_blank">takas.lk</a> තාක්ෂණික සහයොගිත්වයෙන් විපතටපත් ජනතාවට ලබා දෙනු ඇත.
 							මෙතෙක් සිදු කළ දායකත්වයන් <a href="http://pat.as/reliefdata">මෙම සබැදිය</a> භාවිතයෙන් බලා ගත හැක.
 							</p>
 						</li>
 						<li class="list-group-item" style="padding: 2%; font-size: 40px">
 							<p>
-							வெளிநாடுகளில் வசிக்கும் நீங்களும், உங்கள் நன்கொடைகளை IMCD மூலமாக takas.lk உடன் அனுப்பி வைக்க முடியும். <a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">அனுப்பி வைக்க</a>
+							வெளிநாடுகளில் வசிக்கும் நீங்களும், உங்கள் நன்கொடைகளை IMCD மூலமாக takas.lk உடன் அனுப்பி வைக்க முடியும். <a href="http://takas.lk/tag/product/list/tagId/146" target="_blank">அனுப்பி வைக்க</a>
 							</p>
-							<p><a href="http://imcds.org/relief/" target="_blank">IMCD</a> இட்கு இதுவரை அண்ணளவாக Rs 600,000 ஒன்லைன் ஊடக நன்கொடை செய்யப்பட்டுள்ளது. #FloodReliefLKA ஊடக மேலதிக முயற்ச்சிகளும் மேற்கொள்ளப்பட்டுள்ளன. இதுவைர அளிக்கப்பட நன்கொடை சம்பந்தமான மேலதிக விபரங்களுக்கு இங்கே <a href="http://takas.lk/tag/product/list/tagId/196" target="_blank">click</a> செய்யவும்.
+							<p><a href="http://imcds.org/relief/" target="_blank">IMCD</a> இட்கு இதுவரை அண்ணளவாக Rs 600,000 ஒன்லைன் ஊடக நன்கொடை செய்யப்பட்டுள்ளது. #FloodReliefLKA ஊடக மேலதிக முயற்ச்சிகளும் மேற்கொள்ளப்பட்டுள்ளன. இதுவைர அளிக்கப்பட நன்கொடை சம்பந்தமான மேலதிக விபரங்களுக்கு இங்கே <a href="http://takas.lk/tag/product/list/tagId/146" target="_blank">click</a> செய்யவும்.
 						</li>
 					</ul>
 			</div>
